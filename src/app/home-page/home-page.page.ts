dda01b92e6ecebc0555ab71b5913689c2fa7d103
unknown <seifertm@gmx.at>
2021-03-15 16:46:36 +0100

    PickerCreateNotFinished

--- src/app/home-page/home-page.page.ts
+++ src/app/home-page/home-page.page.ts
@@ -1,58 +1,62 @@
 import { Component, OnInit } from '@angular/core';
-import { ActionSheetController } from '@ionic/angular';
+import { ActionSheetController, PickerController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.page.html',
   styleUrls: ['./home-page.page.scss'],
 })
+
 export class HomePagePage implements OnInit {
 
-  constructor(public actionSheetController: ActionSheetController) {}
-
-  
-
-  async presentActionSheet() {
-    const actionSheet = await this.actionSheetController.create({
-      header: 'Dings',
-      cssClass: 'home-page.scss',
-      buttons: [{
-        text: 'Delete',
-        role: 'destructive',
-        icon: 'trash',
-        handler: () => {
-          console.log('Delete clicked');
-        }
-      }, {
-        text: 'Share',
-        icon: 'share',
-        handler: () => {
-          console.log('Share clicked');
-        }
-      }, {
-        text: 'Play (open modal)',
-        icon: 'caret-forward-circle',
-        handler: () => {
-          console.log('Play clicked');
-        }
-      }, {
-        text: 'Favorite',
-        icon: 'heart',
-        handler: () => {
-          console.log('Favorite clicked');
-        }
-      }, {
-        text: 'Cancel',
-        icon: 'close',
-        role: 'cancel',
-        handler: () => {
-          console.log('Cancel clicked');
-        }
-      }]
-    });
-    await actionSheet.present();
+  constructor(public pickerController: PickerController) {
+    this.options = [
+      'Happy',
+      'Sad',
+      'Angry'
+    ];
   }
 
+  Interface
+
+  options: string[];
+  
+  getColumOptions(i){
+    let options = [];
+    
+  
+  }
+
+  getColumns(){
+    let columns = [];
+
+    for(let i = 0; i < this.options.length; i++){
+      columns.push({
+        name: 'col-${i}',
+        options: this.options[i]
+      })
+    }
+
+    return columns;
+  }
+
+
+  async openPicker(){
+    const picker = await this.pickerController.create({
+      columns: this.getColumns(),
+      buttons: [{
+        text: 'Cancel',
+        role: 'cancel'
+      }, {
+        text: 'Confirm',
+        handler: (value) => {
+          console.log('Value: ${value}');
+        }
+      }]
+    })
+
+    await picker.present();
+  }
 
   ngOnInit() {
   }
