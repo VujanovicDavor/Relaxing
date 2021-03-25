d584ffb8ce09de9dbec6b20b72faf400506a7551
VujanovicDavor <55189468+VujanovicDavor@users.noreply.github.com>
2021-03-25 20:17:28 +0100

    Small Update to Protokol

--- src/app/home-page/home-page.page.ts
+++ src/app/home-page/home-page.page.ts
@@ -17,19 +17,11 @@ export class HomePagePage implements OnInit {
     ];
   }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 0208a46cce0e2de9969c763ea31eba26b9937258
   options: string[];
 
   dateTime: Date;
   
-<<<<<<< HEAD
   getColumOptions(){ // returns the options
-=======
-  getColumOptions(){ //returns the options of a column
->>>>>>> 0208a46cce0e2de9969c763ea31eba26b9937258
     let returnOptions = [];
     
     for (let i = 0; i < this.options.length; i++) { //runs through options array
@@ -41,11 +33,7 @@ export class HomePagePage implements OnInit {
       return returnOptions;
     }
 
-<<<<<<< HEAD
-  getColumns(){ //gets options per column (just one column)
-=======
   getColumns(){ // returns the columns with options
->>>>>>> 0208a46cce0e2de9969c763ea31eba26b9937258
     let columns = [];
 
     columns.push({
@@ -57,11 +45,7 @@ export class HomePagePage implements OnInit {
   }
 
 
-<<<<<<< HEAD
   async openPicker(){ //picker round with controller
-=======
-  async openPicker(){ //opens or creates the picker with the options
->>>>>>> 0208a46cce0e2de9969c763ea31eba26b9937258
     const picker = await this.pickerController.create({
       columns: this.getColumns(),
       buttons: [{
@@ -76,14 +60,7 @@ export class HomePagePage implements OnInit {
     })
     
 
-<<<<<<< HEAD
     await picker.present(); //presents and opens picker
-=======
-    //checking here if mood is already inserted!!
-    //else dont present the picker
-    await picker.present(); //presents the picker
-    console.log(this.dateTime = new Date());
->>>>>>> 0208a46cce0e2de9969c763ea31eba26b9937258
   }
 
   ngOnInit() {
